e834f6171d1834287f8497e2f69e27542834f0e4
Alan Plum <me@pluma.io>
2015-07-15 16:52:27 +0200

    Added schemas. Added stub implementation of auth/changepw.

--- api.js
+++ api.js
@@ -3,10 +3,12 @@
 const Unauthorized = require('http-errors').Unauthorized;
 const Foxx = require('org/arangodb/foxx');
 const users = require('./users');
+const schemas = require('./schemas');
+const sessions = applicationContext.dependencies.sessions.sessionStorage;
 const ctrl = new Foxx.Controller(applicationContext);
 
 ctrl.activateSessions({
-  sessionStorage: applicationContext.dependencies.sessions.sessionStorage,
+  sessionStorage: sessions,
   header: true
 });
 
@@ -49,7 +51,7 @@ ctrl.delete('/:userId', function (req, res) {
 ctrl.put('/:userId/authenticate', function (req, res) {
   const userId = req.params('userId');
   const user = user.get(userId);
-  ...
+  // TODO verify password
   res.status(200);
   res.json(user.get('userData'));
 })
@@ -62,7 +64,8 @@ ctrl.put('/:userId/change-password', function (req, res) {
     throw new Unauthorized();
   }
   const user = users.get(userId);
-  ...
+  // TODO change password
+  user.save();
   res.status(204);
 })
 .bodyParam('passwords', schemas.passwordChange)
---
+++ schemas.js
@@ -0,0 +1,17 @@
+/*global require, exports */
+'use strict';
+const joi = require('joi');
+
+exports.userId = joi.string().required()
+.description('Username.');
+exports.userData = joi.object()
+.default(Object, 'empty object');
+exports.password = joi.object().required()
+.keys({password: joi.string().required()})
+.description('Password.');
+exports.passwordChange = joi.object().required()
+.keys({
+  old: joi.string().required(),
+  new: joi.string().required()
+})
+.description('Old and new password.');
